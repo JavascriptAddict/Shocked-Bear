24596623d8fc7dffca0462509a1e33f08a845ea5
JavascriptAddict <44302894+JavascriptAddict@users.noreply.github.com>
2019-10-24 11:46:07 +0800

    Delete index.js

--- art1/index.js
+++
@@ -1,8 +0,0 @@
-// CSS by Mario L
-function move(){
-
-    $(".iris-left").css({"top": "calc(50% - 115px)", "left": "calc(50% - 165px)"});
-    $(".iris-right").css({"top": "calc(50% - 115px)", "right": "calc(50% - 165px)"});
-    $("#mouth").css({"width": "200px", "height": "90px", "border-radius": "50px"});
-
-}
